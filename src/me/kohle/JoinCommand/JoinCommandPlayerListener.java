7ceb6cb3cc425add1ce6916d241c61a775d0f0ad
Kohle <webmaster@penguinzoom.com>
2012-01-13 12:32:38 -0500

    Fixed a little bit, nothing major.

---
+++ src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
@@ -0,0 +1,56 @@
+package me.kohle.JoinCommand;
+
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+
+
+
+@SuppressWarnings("unused")
+public class JoinCommandPlayerListener extends PlayerListener {
+
+	public JoinCommand plugin;
+  	
+
+    
+
+	public JoinCommandPlayerListener(JoinCommand instance) {
+		plugin = instance;
+	}
+	
+	public void filterCommand(Player player , String listType){
+		List<Object> stuff = plugin.getConfig().getList(listType);
+        if(!(stuff == null)){
+        	for (Object s : stuff) {
+        		player.performCommand((String) s); 
+        		//Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), (String) s);
+        	}
+        }
+	}
+	
+	@Override
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+        String name = player.getName();
+        
+		if(player == null) { 	//If the player is not present when the player joins...
+			System.out.println("[JoinCommand] A non-player joined the server. No commands forced.");
+		}
+
+        if(!player.hasPlayedBefore() && player.hasPermission("JoinCommand.FirstCommand")) {
+
+        	String listType = "First Command";   //Call to get First Commands if its the players first time logging on
+            filterCommand(player , listType);
+        	System.out.println( "[JoinCommand] The player " + name + " logged in for first time. " );
+        }
+        if(player.hasPermission("JoinCommand.NormalCommand")) {
+    		String listType = "Commands";
+    		filterCommand(player , listType);
+        }
+
+	}
+}
